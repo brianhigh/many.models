64165b156681d47f8fe4a1cdda8d55ff737b6277
Brian High <high@uw.edu>
2020-05-07 17:54:51 -0700

    Better variable naming.

--- inst/extscripts/mc.boot.alternative.example.R
+++ inst/extscripts/mc.boot.alternative.example.R
@@ -14,8 +14,8 @@ nboot <- 4
 formulas <- c('mpg ~ cyl', 'mpg ~ cyl + disp', 'mpg ~ cyl + disp + hp')
 
 stat.fun <- function(data, ind, formula, model.fun, elem, ...) {
-  ind <- sample(ind, replace = TRUE)
-  do.call(model.fun, list(formula, data[ind,], ...))[[elem]]
+  rand.ind <- sample(ind, replace = TRUE)
+  do.call(model.fun, list(formula, data[rand.ind,], ...))[[elem]]
 }
 
 clean.fun <- function(df, f) {
